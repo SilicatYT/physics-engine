cb082cccaccfe10c06c73e657ddf38151d36376e
SilicatYT <silicat.yt@gmx.ch>
2025-11-25 19:47:06 +0100

    Improve data structure
    - Renamed "size" to "scale"
    - Renamed "updateInverseInertiaTensor...()" to "updateInertiaTensor...()" because the information that it's the inverse isn't important (A non-inverse inertia tensor doesn't exist here)
    - All Vector3d and Quaternion3d are now stored in NBT using doubles instead of floats for better precision
    - Added a method "initializeObjectData()" that replaces "updateAllTransientData()" with more functionality
    - Physics objects now have teleport_duration and interpolation_duration of 1

--- physics-engine-reference-1.21.10/src/main/java/silicatyt/physicsref/entity/PhysicsObject.java
+++ physics-engine-reference-1.21.10/src/main/java/silicatyt/physicsref/entity/PhysicsObject.java
@@ -7,19 +7,20 @@ import net.minecraft.entity.decoration.DisplayEntity.ItemDisplayEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.storage.ReadView;
 import net.minecraft.storage.WriteView;
-import net.minecraft.util.dynamic.Codecs;
 import net.minecraft.util.math.AffineTransformation;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.joml.*;
 import xyz.nucleoid.packettweaker.PacketContext;
 
+import java.util.List;
+
 
 // Note: All doubles could be NaN, Infinity or -Infinity, but I don't check for that everywhere. I don't have a consequent rule for that. I could check for isFinite() on the vectors and quaternions (on quaternions I could additionally check if it's all 0; I can't normalize that), but there's not really a point because this isn't meant to be an official well-made mod. Just a reference.
 
 public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
     public static final double DEFAULT_INVERSE_MASS = 0.001d;
-    public static final Vector3d DEFAULT_SIZE = new Vector3d(1d, 1d, 1d);
+    public static final Vector3d DEFAULT_SCALE = new Vector3d(1d, 1d, 1d);
     public static final double DEFAULT_FRICTION_COEFFICIENT = 0.5d;
     public static final double DEFAULT_RESTITUTION_COEFFICIENT = 0.3d;
 
@@ -28,7 +29,7 @@ public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
     private Vector3d linearVelocity;
     private Vector3d angularVelocity;
     private Quaterniond orientation;
-    private Vector3d size;
+    private Vector3d scale;
     private double frictionCoefficient;
     private double restitutionCoefficient;
 
@@ -51,11 +52,11 @@ public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
         this.linearVelocity = new Vector3d();
         this.angularVelocity = new Vector3d();
         this.orientation = new Quaterniond();
-        this.size = DEFAULT_SIZE;
+        this.scale = DEFAULT_SCALE;
         this.frictionCoefficient = DEFAULT_FRICTION_COEFFICIENT;
         this.restitutionCoefficient = DEFAULT_RESTITUTION_COEFFICIENT;
 
-        this.updateAllTransientData();
+        this.initializeObjectData();
     }
 
     // Getters & Setters
@@ -72,7 +73,7 @@ public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
             throw new IllegalArgumentException("Mass must not be negative");
         }
         this.inverseMass = value;
-        this.updateInverseInertiaTensors();
+        this.updateInertiaTensors();
     }
 
     public void setMass(double value) throws IllegalArgumentException {
@@ -112,24 +113,24 @@ public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
         this.orientation = value.normalize();
 
         this.updateRotationMatrix();
-        this.updateInverseInertiaTensorWorld(); // Requires the updated rotation matrix
+        this.updateInertiaTensorWorld(); // Requires the updated rotation matrix
         this.updateVisualTransformation();
     }
 
-    public Vector3d getSize() {
-        return this.size;
+    public Vector3d getScale() {
+        return this.scale;
     }
 
-    public void setSize(Vector3d value) throws IllegalArgumentException {
+    public void setScale(Vector3d value) throws IllegalArgumentException {
         if (value == null) {
             throw new IllegalArgumentException("Vector must not be null");
         }
         if (value.x < 0 || value.y < 0 || value.z < 0) {
-            throw new IllegalArgumentException("Size must not be negative");
+            throw new IllegalArgumentException("Scale must not be negative");
         }
-        this.size = value;
+        this.scale = value;
 
-        this.updateInverseInertiaTensors();
+        this.updateInertiaTensors();
         this.updateVisualTransformation();
     }
 
@@ -159,15 +160,16 @@ public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
     @Override
     protected void writeCustomData(WriteView view) { // I can't store doubles properly, so I have to convert to float here
         // super.writeCustomData(view); // <- Completely ignore the item display's NBT: I'll take over from here, thanks. I can't read the orientation and stuff anyway (requiring me to keep track of them myself), so I might as well. Makes the data prettier if useless data isn't in there.
+        Codec<List<Double>> doubleListCodec = Codec.DOUBLE.listOf();
 
         if (!this.getItemStack().isEmpty()) { // ItemStack.Codec doesn't work for empty items
             view.put("item", ItemStack.CODEC, this.getItemStack());
         }
         view.putDouble("inverse_mass", inverseMass);
-        view.put("linear_velocity", Codecs.VECTOR_3F, new Vector3f(linearVelocity));
-        view.put("angular_velocity", Codecs.VECTOR_3F, new Vector3f(angularVelocity));
-        view.put("orientation", Codecs.QUATERNION_F, new Quaternionf(orientation));
-        view.put("size", Codecs.VECTOR_3F, new Vector3f(size)); // size and the velocities are stored in floats (because there is no VECTOR_3D codec), but is a double at runtime to avoid excessive casting
+        view.put("linear_velocity", doubleListCodec, List.of(linearVelocity.x, linearVelocity.y, linearVelocity.z));
+        view.put("angular_velocity", doubleListCodec, List.of(angularVelocity.x, angularVelocity.y, angularVelocity.z));
+        view.put("orientation", doubleListCodec, List.of(orientation.x, orientation.y, orientation.z, orientation.w));
+        view.put("scale", doubleListCodec, List.of(scale.x, scale.y, scale.z));
         view.putDouble("friction_coefficient", frictionCoefficient);
         view.putDouble("restitution_coefficient", restitutionCoefficient);
     }
@@ -175,18 +177,22 @@ public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
     @Override
     protected void readCustomData(ReadView view) { // ALL required instance variables need to be set after this, so I need to also calculate the rotationMatrix from the orientation for example. Otherwise it will be null after unloading and loading again, causing all sorts of calculation bugs.
         // super.readCustomData(view);
+        Codec<List<Double>> doubleListCodec = Codec.DOUBLE.listOf();
 
         this.setItemStack(view.read("item", ItemStack.CODEC).orElse(ItemStack.EMPTY));
         this.inverseMass = view.read("inverse_mass", Codec.DOUBLE).orElse(DEFAULT_INVERSE_MASS);
-        this.linearVelocity = new Vector3d(view.read("linear_velocity", Codecs.VECTOR_3F).orElse(new Vector3f()));
-        this.angularVelocity = new Vector3d(view.read("angular_velocity", Codecs.VECTOR_3F).orElse(new Vector3f()));
-        this.orientation = new Quaterniond(view.read("orientation", Codecs.QUATERNION_F).orElse(new Quaternionf()));
-        this.size = new Vector3d(view.read("size", Codecs.VECTOR_3F).orElse(new Vector3f()));
+        List<Double> linearVelocity = view.read("linear_velocity", doubleListCodec).orElse(List.of(0d, 0d, 0d));
+        this.linearVelocity = new Vector3d(linearVelocity.get(0), linearVelocity.get(1), linearVelocity.get(2));
+        List<Double> angularVelocity = view.read("angular_velocity", doubleListCodec).orElse(List.of(0d, 0d, 0d));
+        this.angularVelocity = new Vector3d(angularVelocity.get(0), angularVelocity.get(1), angularVelocity.get(2));
+        List<Double> orientation = view.read("orientation", doubleListCodec).orElse(List.of(0d, 0d, 0d, 1d));
+        this.orientation = new Quaterniond(orientation.get(0), orientation.get(1), orientation.get(2), orientation.get(3));
+        List<Double> scale = view.read("scale", doubleListCodec).orElse(List.of(0d, 0d, 0d));
+        this.scale = new Vector3d(scale.get(0), scale.get(1), scale.get(2));
         this.frictionCoefficient = view.read("friction_coefficient", Codec.DOUBLE).orElse(DEFAULT_FRICTION_COEFFICIENT);
         this.restitutionCoefficient = view.read("restitution_coefficient", Codec.DOUBLE).orElse(DEFAULT_RESTITUTION_COEFFICIENT);
 
-        this.updateAllTransientData();
-        this.updateVisualTransformation();
+        this.initializeObjectData();
     }
 
     // Normal methods
@@ -195,46 +201,43 @@ public class PhysicsObject extends ItemDisplayEntity implements PolymerEntity {
         return EntityType.ITEM_DISPLAY;
     }
 
-    private void updateAllTransientData() { // Makes sure that no instance variables are unset after instantiation or getting loaded from disk. Some data is updated twice unfortunately.
+    private void initializeObjectData() { // Makes sure that all instance variables are initialized (Not NULL) and update the visual transformation upon getting loaded or instantiated
         Vec3d entityPos = this.getEntityPos();
         this.pos = new Vector3d(entityPos.x, entityPos.y, entityPos.z);
+        this.setInterpolationDuration(1);
+        this.setStartInterpolation(0);
+        this.setTeleportDuration(1);
 
-        if (this.accumulatedForce == null) {
-            this.accumulatedForce = new Vector3d();
-        }
-        if (this.accumulatedTorque == null) {
-            this.accumulatedTorque = new Vector3d();
-        }
+        this.accumulatedForce = new Vector3d();
+        this.accumulatedTorque = new Vector3d();
+        this.rotationMatrix = new Matrix3d();
+        this.inverseInertiaTensorLocal = new Matrix3d();
 
-        this.updateRotationMatrix();
-        this.updateInverseInertiaTensors();
+        this.updateRotationMatrix(); // Requires rotationMatrix to not be NULL
+        this.updateInertiaTensors(); // Requires inverseInertiaTensorLocal to not be NULL
+        this.updateVisualTransformation();
     }
 
     private void updateVisualTransformation() {
-        this.setTransformation(new AffineTransformation(new Vector3f(), new Quaternionf(this.orientation), new Vector3f(this.size), new Quaternionf()));
+        this.setStartInterpolation(0);
+        this.setTransformation(new AffineTransformation(new Vector3f(), new Quaternionf(this.orientation), new Vector3f(this.scale), new Quaternionf())); // Although I calculate everything in doubles, the rendering uses floats because that's how item displays (and Minecraft's rendering in general) work
     }
 
     private void updateRotationMatrix() {
-        if (this.rotationMatrix == null) { // I don't want to have a dedicated updateRotationMatrix method and then still have to initialize it in the "object got loaded" or "new object got created" methods. Maybe it's unoptimized, but it's more readable imo
-            this.rotationMatrix = new Matrix3d();
-        }
         this.orientation.get(this.rotationMatrix);
         this.rotationMatrixTranspose = this.rotationMatrix.transpose();
     }
 
-    private void updateInverseInertiaTensorWorld() {
+    private void updateInertiaTensorWorld() {
         this.inverseInertiaTensorGlobal = this.inverseInertiaTensorLocal.mul(this.rotationMatrix);
     }
 
-    private void updateInverseInertiaTensors() {
-        if (this.inverseInertiaTensorLocal == null) { // I don't want to have a dedicated updateInverseInertiaTensors method and then still have to initialize it in the "object got loaded" or "new object got created" methods. Maybe it's unoptimized, but it's more readable imo
-            this.inverseInertiaTensorLocal = new Matrix3d();
-        }
-        this.inverseInertiaTensorLocal.m00 = (12 * this.inverseMass) / (this.size.y * this.size.y + this.size.z * this.size.z);
-        this.inverseInertiaTensorLocal.m11 = (12 * this.inverseMass) / (this.size.x * this.size.x + this.size.z * this.size.z);
-        this.inverseInertiaTensorLocal.m22 = (12 * this.inverseMass) / (this.size.x * this.size.x + this.size.y * this.size.y);
+    private void updateInertiaTensors() {
+        this.inverseInertiaTensorLocal.m00 = (12 * this.inverseMass) / (this.scale.y * this.scale.y + this.scale.z * this.scale.z);
+        this.inverseInertiaTensorLocal.m11 = (12 * this.inverseMass) / (this.scale.x * this.scale.x + this.scale.z * this.scale.z);
+        this.inverseInertiaTensorLocal.m22 = (12 * this.inverseMass) / (this.scale.x * this.scale.x + this.scale.y * this.scale.y);
 
-        this.updateInverseInertiaTensorWorld();
+        this.updateInertiaTensorWorld();
     }
 
     // TODO: Add more public methods for manipulating the physics object
